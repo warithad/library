5b04f37c8316123712bd1b48ce945453c073167f
warith <warith.ad02@gmail.com>
2022-10-08 00:24:45 +0100

    Complete styling and background layout

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let library = [];
+
+function Book(title, author, pages){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = false;
+}
+
+function addBookToLibrary(Book){
+    library.push(book);
+}
+
+
+const main = document.querySelector(".main");
