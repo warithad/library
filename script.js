b242ac093b77b5f1bb8cea1b77e8291f95341d89
warith <warith.ad02@gmail.com>
2022-10-08 03:40:02 +0100

    Complete total layout and javascript main functionality

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const main = document.querySelector(".main");
+const modal = document.getElementById('modal');
+const openModal = document.getElementById('add-book');
+
+
+const form = document.getElementById('create-book');
+const bookTitle = document.getElementById('title');
+const bookAuthor = document.getElementById('author');
+const bookPages = document.getElementById('pages');
+const submit = document.getElementById('submit-book');
 
 let library = [];
 
@@ -8,9 +18,58 @@ function Book(title, author, pages){
     this.read = false;
 }
 
-function addBookToLibrary(Book){
+function addBookToLibrary(book){
     library.push(book);
+    
+    
+    const div = createDiv(book);
+    div.classList.add('card');
+    main.appendChild(div);
 }
 
+openModal.addEventListener('click', ()=>{
+    modal.showModal();
+})
 
-const main = document.querySelector(".main");
+// creates card div
+function createDiv(book){
+    const div = document.createElement('div');
+    
+    const t = document.createElement('h1');
+    t.classList.add('card-title');
+
+    const a = document.createElement('h2');
+    a.classList.add('card-author');
+    
+    const p = document.createElement('h2');
+    p.classList.add('card-pages');
+
+    const but = document.createElement('button');
+    but.classList.add('card-button');
+    
+    but.textContent = 'Read';
+    t.textContent = book.title.toUpperCase();
+    a.textContent = 'Author: ' + book.author;
+    p.textContent = 'Pages: '+book.pages;
+
+    div.appendChild(t);
+    div.appendChild(a);
+    div.appendChild(p);
+    div.appendChild(but);
+
+    console.log(div);
+    return div;
+}
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const book = new Book(bookTitle.value, bookAuthor.value, bookPages.value);
+    console.log(book + ' created');
+    addBookToLibrary(book);
+    modal.close();
+})
+
+window.onload =()=>{
+    const demoBook = new Book('Hocus Pocus', 'Me', '200');
+    addBookToLibrary(demoBook);
+}
